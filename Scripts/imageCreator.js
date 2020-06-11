e5a1814a6054ff194c563b7e8b9be63404a01c70
Heath Burton <rubickdev@gmail.com>
2020-06-11 12:41:17 +1000

    Stuck on Image displaying

--- Scripts/imageCreator.js
+++ Scripts/imageCreator.js
@@ -1,8 +1,19 @@
-function InsertImageSlices {
+
+
+function InsertImageSlices() {
+  let dispImg = "";
+  let dirName = "../../images/slicedimages/"
+  let fileName= "bcpot002_r"
   let row, col;
-  for(let row = 0; row < 5; row++){
+  
+  for(let row = 0; row < 4; row++){
+    dispImg += "<tr>"
     for(let col = 0; col <  5; col++){
-      
+      dispImg += "<td>"
+      dispImg += `<img src="${dirName}${fileName}${row+1}_c${col+1}.jpg"</td>`
     }
+    dispImg += "</tr>"
+  document.getElementById('imageSlices').innerHTML = dispImg;
   }
 }
+
